621ef966e99be34c33bed028e03b875ce0a59d83
gourav <gourav96suri@gmail.com>
2017-02-04 23:19:35 +0530

    Initial Commit

---
+++ Waiting Thread.java
@@ -0,0 +1,28 @@
+//Below Code is to update UI with every 10 seconds
+//First declare one global varialbe for Handler to update the UI control from Thread, like below
+
+Handler mHandler = new Handler();
+
+//Now create one Thread and use while loop to periodically perform the task using the sleep method of the thread.
+
+ new Thread(new Runnable() {
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            while (true) {
+                try {
+                    Thread.sleep(10000); //Sleep for 10 seconds
+                    mHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            // Write your code here to update the UI.
+                        }
+                    });
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+            }
+        }
+    }).start();
